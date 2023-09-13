7bd2215cdb639dd8cc90323a46919b171c5b3eb8
Pedro de Souza Martins <143435476+SinPedro@users.noreply.github.com>
2023-09-13 14:58:56 -0300

    Add files via upload

---
+++ maioridade/idade.js
@@ -0,0 +1,16 @@
+// digitando a idade
+
+let idade = Number(prompt("Digite a sua idade:"))
+
+
+// calculando a maioridade
+
+
+if (idade >= 18) {
+    console.log("VOCE É MAIOR DE IDADE");
+}
+else {
+    console.log("VOCÊ AINDA TEM MUITO A CRESCER");
+}
+
+
